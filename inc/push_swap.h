89196e76a204d540c9c1de6e94eb10572ec92e5f
Nathanael PINEAU <npineau@student.42.fr>
2017-11-30 08:10:18 +0100

    let's not depended on the order of inclusions ok ?

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,15 +6,17 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 14:47:50 by npineau           #+#    #+#             */
-/*   Updated: 2017/11/28 11:38:24 by npineau          ###   ########.fr       */
+/*   Updated: 2017/11/30 08:09:57 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-int	args_to_stack(size_t n, char **args, t_rb *a);
+# include "libft/inc/libft.h"
+
+int	args_to_stack(size_t n, t_str *args, t_rb *a);
 int	get_program(t_rb *instructions);
-int	parse_int(t_string in, t_string *out, int *integer);
+int	parse_int(t_str in, t_str *out, int *integer);
 
 #endif
